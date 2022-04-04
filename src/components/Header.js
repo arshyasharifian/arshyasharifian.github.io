fdf9479583355ee2b63ac85e7841a54d9fb563da
arshyasharifian <arshyasharifian@users.noreply.github.com>
2022-04-04 04:01:31 +0000

    deploy: 3c8a177cd74c7531d487701309c4bc0befe981b1

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-class Header extends React.Component {  
-    render() {
-        return <h1 className='myHeader'>Arshya "Ary" Sharifian</h1>
-    }
-}
-
-export default Header
