0cc6446fc1f1d5534cf60aa1d4a0742de4ec3f2d
Arshya Sharifian <arshyasharifian@gmail.com>
2022-04-09 20:45:17 -0700

    adding some text

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react'
 import Header from './components/Header';
 import Links from './components/Links';
+import Textbox from './components/Textbox'
 
 function App() {
   return (
     <>
     <Header />
     <Links />
+    <Textbox />
     </>
   );
 }
---
+++ src/components/Textbox.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Textbox extends React.Component {  
+    render() {
+        return <p1 className='myTextbox'>This my website. There are many like it, but this one is mine.</p1>
+    }
+}
+
+export default Textbox
